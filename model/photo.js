fce474428e7b211be182ef165398bc628b6d8922
Steven Carpenter <stevegcarpenter@gmail.com>
2018-02-08 21:24:04 -0800

    Adding project files

---
+++ model/photo.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const fs = require('fs');
+const del = require('del');
+const path = require('path');
+const Car = require('./car');
+const mongoose = require('mongoose');
+const tempDir = `${__dirname}/../temp`;
+const awsS3 = require('../lib/aws-s3');
+
+const Photo = mongoose.Schema({
+  name: {type: String, required: true},
+  desc: {type: String, required: true},
+  userId: {type: mongoose.Schema.Types.ObjectId, ref: 'auth', required: true},
+  carId: {type: mongoose.Schema.Types.ObjectId, ref: 'car', required: true},
+  objectKey: {type: String, required: true, unique: true},
+  imageURI: {type: String, required: true, unique: true},
+});
+
+Photo.statics.upload = function (req) {
+  return new Promise((resolve, reject) => {
+    if (!req.file) return reject(new Error('Multi-part Form Data Error. File not present on request.'));
+    if (!req.file.path) return reject(new Error('Multi-part Form Data Error. File path not present on request.'));
+
+    let params = {
+      ACL: 'public-read',
+      Bucket: process.env.AWS_BUCKET,
+      Key: `${req.file.filename}${path.extname(req.file.originalname)}`,
+      Body: fs.createReadStream(req.file.path),
+    };
+
+    return awsS3.uploadProm(params)
+      .then(data => {
+        del([`${tempDir}/${req.file.filename}`]);
+
+        let photoData = {
+          name: req.body.name,
+          desc: req.body.desc,
+          userId: req.user._id,
+          carId: req.body.carId,
+          imageURI: data.Location,
+          objectKey: data.Key,
+        };
+
+        resolve(photoData);
+      })
+      .catch(reject)
+  });
+};
+
+// Photo.methods.delete = function () {}
+
+module.exports = mongoose.model('photo', Photo);
